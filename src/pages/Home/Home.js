a1c31da7a23cc3c04016b97e707fcfbfb2291db9
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2026-01-04 22:00:22 -0300

    zxc

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -92,7 +92,7 @@ export default function Home() {
 
 {/* CARROSSEL */}
 <section className="home-carousel home-card">
-  <CCarousel controls indicators interval={4000} className="carousel-custom">
+  <CCarousel controls indicators interval={3000} className="carousel-custom">
     {carouselImages.map((img, index) => (
       <CCarouselItem key={index}>
         <div className="slide-wrapper">
