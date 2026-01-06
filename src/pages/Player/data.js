ca1884cee1bb6bbdb41ca0493df03c741d8e05d6
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2026-01-05 21:16:45 -0300

    hju

--- src/pages/Player/data.js
+++ src/pages/Player/data.js
@@ -3,8 +3,8 @@ import { v4 as uuidv4 } from "uuid";
 
 const data = () => [
   {
-    name: "Deus de Detalhes",
-    artist: "Aso, Middle School",
+    name: "O Tempo Não Pode Apagar",
+    artist: "Cassiane e Jairinho",
     cover: "/images/2.png",
     audio: "/music/a.mp3",
     color: ["#205950", "#2ab3bf"],
@@ -14,17 +14,26 @@ const data = () => [
   {
     name: "Bondade de Deus",
     artist: "Isaías Saad",
-    cover: "https://i.scdn.co/image/ab67616d0000b2739e1cfc756886e3c4d7e9923e",
+    cover: "/images/img.png",
     audio: "/music/bondade-de-deus.mp3",
     color: ["#EF8EA9", "#ab417f"],
     id: uuidv4(),
     active: false,
   },
   {
-    name: "Oceans",
-    artist: "Hillsong United",
-    cover: "https://i.scdn.co/image/ab67616d0000b273e9a9a8e430d4570f6d37dc41",
-    audio: "", // FUNCIONA 100%
+    name: "Quem_E_Esse",
+    artist: "Julliany_Souza",
+    cover: "/images/2.gif",
+    audio: "/music/Quem_E_Esse.mp3", // FUNCIONA 100%
+    color: ["#84fab0", "#8fd3f4"],
+    id: uuidv4(),
+    active: false,
+  },
+  {
+    name: "Deus_de_Obras_Completas",
+    artist: "Kemilly_Santos",
+    cover: "/images/1.gif",
+    audio: "/music/Deus_de_Obras_Completas.mp3", // FUNCIONA 100%
     color: ["#84fab0", "#8fd3f4"],
     id: uuidv4(),
     active: false,
